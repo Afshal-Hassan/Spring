d3113b207d6709928889cf4490306a8b42eb8fe7
Afshal1 <afshalhassan7@gmail.com>
2022-08-14 19:06:50 +0500

    Updated Version

--- src/main/java/com/example/restapis/Controllers/LoginController.java
+++ src/main/java/com/example/restapis/Controllers/LoginController.java
@@ -1,7 +1,9 @@
 package com.example.restapis.Controllers;
+import com.example.restapis.Service.CustomUserDetailsService;
 import com.example.restapis.Service.LoginDataServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,14 +14,21 @@ public class LoginController {
 
     @Autowired
     LoginDataServiceImpl loginDataService;
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
 
-
+    @CrossOrigin
     @GetMapping("/loginData/{email}")
     public List<String> loginData(@PathVariable("email")String email){
       return loginDataService.findLoginData(email);
     }
 
+    @PostMapping("/dosignin")
+    public UserDetails checkLogin(@RequestBody String email){
+        return  customUserDetailsService.loadUserByUsername(email);
 
+
+    }
 
 
 
